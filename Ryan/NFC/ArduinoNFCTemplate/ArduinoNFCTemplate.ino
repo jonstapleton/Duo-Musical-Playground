c73ce193b26ecf09b4f7cb5d4fc117224b6e5f12
staplejm <staplejm@dukes.jmu.edu>
2017-12-13 11:59:58 -0500

    ArduinoNFCTeensyPinAssignments

--- Ryan/NFC/ArduinoNFCTemplate/ArduinoNFCTemplate.ino
+++ Ryan/NFC/ArduinoNFCTemplate/ArduinoNFCTemplate.ino
@@ -2,8 +2,7 @@
 /*! 
     @file     readMifare.pde
     @author   Adafruit Industries
-	@license  BSD (see license.txt)
-
+  @license  BSD (see license.txt)
     This example will wait for any ISO14443A card or tag, and
     depending on the size of the UID will attempt to read from it.
    
@@ -14,24 +13,20 @@
       the default KEYA of 0XFF 0XFF 0XFF 0XFF 0XFF 0XFF
     - If authentication succeeds, we can then read any of the
       4 blocks in that sector (though only block 4 is read here)
-	 
+   
     If the card has a 7-byte UID it is probably a Mifare
     Ultralight card, and the 4 byte pages can be read directly.
     Page 4 is read by default since this is the first 'general-
     purpose' page on the tags.
-
-
 This is an example sketch for the Adafruit PN532 NFC/RFID breakout boards
 This library works with the Adafruit NFC breakout 
   ----> https://www.adafruit.com/products/364
  
 Check out the links above for our tutorials and wiring diagrams 
 These chips use SPI or I2C to communicate.
-
 Adafruit invests time and resources providing this open source code, 
 please support Adafruit and open-source hardware by purchasing 
 products from Adafruit!
-
 */
 /**************************************************************************/
 #include <Wire.h>
@@ -39,10 +34,10 @@ products from Adafruit!
 #include <Adafruit_PN532.h>
 
 // If using the breakout with SPI, define the pins for SPI communication.
-#define PN532_SCK  (2)
-#define PN532_MOSI (3)
-#define PN532_SS   (4)
-#define PN532_MISO (5)
+#define PN532_SCK  (13)
+#define PN532_MOSI (11)
+#define PN532_SS   (10)
+#define PN532_MISO (12)
 
 // If using the breakout or shield with I2C, define just the pins connected
 // to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
@@ -150,37 +145,37 @@ void loop(void) {
     {
       // We probably have a Mifare Classic card ... 
       Serial.println("Seems to be a Mifare Classic card (4 byte UID)");
-	  
+    
       // Now we need to try to authenticate it for read/write access
       // Try with the factory default KeyA: 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
       Serial.println("Trying to authenticate block 4 with default KEYA value");
       uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
-	  
-	  // Start with block 4 (the first block of sector 1) since sector 0
-	  // contains the manufacturer data and it's probably better just
-	  // to leave it alone unless you know what you're doing
+    
+    // Start with block 4 (the first block of sector 1) since sector 0
+    // contains the manufacturer data and it's probably better just
+    // to leave it alone unless you know what you're doing
       success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya);
-	  
+    
       if (success)
       {
         Serial.println("Sector 1 (Blocks 4..7) has been authenticated");
         uint8_t data[16];
-		
+    
         // If you want to write something to block 4 to test with, uncomment
-		// the following line and this text should be read back in a minute
+    // the following line and this text should be read back in a minute
         //memcpy(data, (const uint8_t[]){ 'a', 'd', 'a', 'f', 'r', 'u', 'i', 't', '.', 'c', 'o', 'm', 0, 0, 0, 0 }, sizeof data);
         // success = nfc.mifareclassic_WriteDataBlock (4, data);
 
         // Try to read the contents of block 4
         success = nfc.mifareclassic_ReadDataBlock(4, data);
-		
+    
         if (success)
         {
           // Data seems to have been read ... spit it out
           Serial.println("Reading Block 4:");
           nfc.PrintHexChar(data, 16);
           Serial.println("");
-		  
+      
           // Wait a bit before reading the card again
           delay(1000);
         }
@@ -199,7 +194,7 @@ void loop(void) {
     {
       // We probably have a Mifare Ultralight card ...
       Serial.println("Seems to be a Mifare Ultralight tag (7 byte UID)");
-	  
+    
       // Try to read the first general-purpose user page (#4)
       Serial.println("Reading page 4");
       uint8_t data[32];
@@ -209,7 +204,7 @@ void loop(void) {
         // Data seems to have been read ... spit it out
         nfc.PrintHexChar(data, 4);
         Serial.println("");
-		
+    
         // Wait a bit before reading the card again
         delay(1000);
       }
@@ -220,4 +215,3 @@ void loop(void) {
     }
   }
 }
-
